9c0c1f13d50311da1652cd21dd54856f2cdf5df5
MakigItalia <info@makingitalia.com>
2016-08-01 12:55:14 +0200

    Aggiornamenti

---
+++ ConsoleUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IrcBot
+{
+    /// <summary>
+    /// Console Utility for Drawing, Cursor, graphics and more
+    /// </summary>
+    class ConsoleUtility
+    {
+
+
+        /// <summary>
+        /// Change Console Color
+        /// </summary>
+        /// <param name="ForeGround">Foreground Color</param>
+        /// <param name="Background">Background Color</param>
+        public static void ChangeColor(ConsoleColor ForeGround, ConsoleColor Background)
+        {
+            Console.BackgroundColor = Background;
+            Console.ForegroundColor = ForeGround;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace IrcBot
 {
@@ -16,8 +17,35 @@ namespace IrcBot
 
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            Credit(); //Show Credit
 
+            Console.WriteLine("a");
+            Console.ReadKey();
         }
 
+
+
+        /// <summary>
+        /// Simple void to Draw Credit
+        /// </summary>
+        static void Credit()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine(@"=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o");
+            Console.WriteLine(@"                                                                             ");
+            Console.WriteLine(@" _____            ______                _______             _                ");
+            Console.WriteLine(@"(_____)          (____  \       _      (_______)           (_)               ");
+            Console.WriteLine(@"   _    ____ ____ ____)  ) ___ | |_     _____   ____   ____ _ ____   ____    ");
+            Console.WriteLine(@"  | |  / ___) ___)  __  ( / _ \|  _)   |  ___) |  _ \ / _  | |  _ \ / _  )   ");
+            Console.WriteLine(@" _| |_| |  ( (___| |__)  ) |_| | |__   | |_____| | | ( ( | | | | | ( (/ /    ");
+            Console.WriteLine(@"(_____)_|   \____)______/ \___/ \___)  |_______)_| |_|\_|| |_|_| |_|\____)   ");
+            Console.WriteLine(@"                                                     (_____|                 ");
+            Console.WriteLine(@"                                                                             ");
+            Console.WriteLine(@"=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o,.o=o");
+            Thread.Sleep(2000);
+        }
     }
 }
